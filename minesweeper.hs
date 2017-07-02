626ca34ba4d2498157b04259e420a03d77a4e10f
Aramis <aramis.araujo@ccc.ufcg.edu.br>
2017-07-01 23:03:31 -0300

    Adiciona as constantes do jogo

--- minesweeper.hs
+++ minesweeper.hs
@@ -1,11 +1,22 @@
 import Data.List
 import System.IO
+import System.Random
 
 --Game Tile Constants
 coveredTile = "■"
 emptyTile = "□"
 flaggedTile = "🏴"
 bombTile = "💣"
+
+
+--Game Constants
+boardSize = 8 -- 9x9
+bombDensity = 0.1 -- 10% of the game board
+bombs = getAmountBombs boardSize bombDensity
+
+
+getAmountBombs :: Int -> Float -> Int
+getAmountBombs boardSize bombDensity = round (boardSize * bombDensity) :: Int
 
 main = do
 	putStrLn "Not Yet Implemented"
