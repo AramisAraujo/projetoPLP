298fd6a571750a08706fc82c53ad742c16542d06
Aramis <aramis.araujo@ccc.ufcg.edu.br>
2017-07-04 23:39:55 -0300

    Implementa primeiros passos da impressão

--- minesweeper.hs
+++ minesweeper.hs
@@ -22,10 +22,31 @@ getInitialGameboard boardSize = replicate boardSize (replicate boardSize 0)
 getInitialDisplay :: Int -> [[Char]]
 getInitialDisplay boardSize = replicate boardSize (replicate boardSize coveredTile)
 
+--Printing 
+printBoard :: [[Int]] -> IO()
+printBoard gameBoard = do
+	sequence_ (map print gameBoard) --Sequence will execute the IO() commands that are in the mapping
+
+printDisplay :: [[Char]] -> IO()
+printDisplay displayBoard = do
+	sequence_ (map putStrLn (map prepLine displayBoard))--This external map creates ::[IO()]. Sequence executes those IO commands
+
+prepLine :: String -> String
+prepLine line =  prepLineAux line (length line)
+
+prepLineAux :: String -> Int -> String
+prepLineAux line 1 = line
+prepLineAux (x:xs) len = (x : "  ") ++ prepLineAux xs (length xs)
+
+
 main = do
 	
 	let amountBombs = getAmountBombs boardSize bombDensity
 	let iGameboard = getInitialGameboard boardSize
 	let iDisplay = getInitialDisplay boardSize
+
+	printBoard iGameboard
+
+	printDisplay iDisplay
 
 	putStrLn "Not Yet Implemented"
