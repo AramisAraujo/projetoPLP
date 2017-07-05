a1cb24ef86bb2d20c9a171533ad55d4684c5211b
Aramis Araujo <aramis.araujo@ccc.ufcg.edu.br>
2017-07-04 22:24:45 -0300

    Implementa funções para o campo

--- minesweeper.hs
+++ minesweeper.hs
@@ -1,22 +1,31 @@
 import Data.List
 import System.IO
-import System.Random
+--import System.Random
 
 --Game Tile Constants
-coveredTile = "■"
-emptyTile = "□"
-flaggedTile = "🏴"
-bombTile = "💣"
-
+coveredTile = '■'
+emptyTile = '□'
+flaggedTile = '🏴'
+bombTile = '💣'
 
 --Game Constants
-boardSize = 8 -- 9x9
+boardSize = 9 -- 9x9
 bombDensity = 0.1 -- 10% of the game board
-bombs = getAmountBombs boardSize bombDensity
 
-
+--Game Initial functions
 getAmountBombs :: Int -> Float -> Int
-getAmountBombs boardSize bombDensity = round (boardSize * bombDensity) :: Int
+getAmountBombs boardSize bombDensity = round ( (fromIntegral boardSize) * bombDensity * 10) :: Int
+
+getInitialGameboard :: Int -> [[Int]]
+getInitialGameboard boardSize = replicate boardSize (replicate boardSize 0)
+
+getInitialDisplay :: Int -> [[Char]]
+getInitialDisplay boardSize = replicate boardSize (replicate boardSize coveredTile)
 
 main = do
+	
+	let amountBombs = getAmountBombs boardSize bombDensity
+	let iGameboard = getInitialGameboard boardSize
+	let iDisplay = getInitialDisplay boardSize
+
 	putStrLn "Not Yet Implemented"
