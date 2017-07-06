f8bbfe7c1db3f0a191803800140c9079260740e6
Aramis <aramis.araujo@ccc.ufcg.edu.br>
2017-07-05 21:15:43 -0300

    Adiciona funções de operações com listas(NC)

--- minesweeper.hs
+++ minesweeper.hs
@@ -22,21 +22,41 @@ getInitialGameboard boardSize = replicate boardSize (replicate boardSize 0)
 getInitialDisplay :: Int -> [[Char]]
 getInitialDisplay boardSize = replicate boardSize (replicate boardSize coveredTile)
 
---Printing 
+--Printing related functions
 printBoard :: [[Int]] -> IO()
 printBoard gameBoard = do
 	sequence_ (map print gameBoard) --Sequence will execute the IO() commands that are in the mapping
 
 printDisplay :: [[Char]] -> IO()
 printDisplay displayBoard = do
-	sequence_ (map putStrLn (map prepLine displayBoard))--This external map creates ::[IO()]. Sequence executes those IO commands
+	sequence_ (map putStrLn (map formatLine displayBoard))--This external map creates ::[IO()]. Sequence executes those IO commands
 
-prepLine :: String -> String
-prepLine line =  prepLineAux line (length line)
+formatLine :: String -> String
+formatLine line =  formatLineAux line (length line)
 
-prepLineAux :: String -> Int -> String
-prepLineAux line 1 = line
-prepLineAux (x:xs) len = (x : "  ") ++ prepLineAux xs (length xs)
+formatLineAux :: String -> Int -> String
+formatLineAux line 1 = line
+formatLineAux (x:xs) len = (x : "  ") ++ formatLineAux xs (length xs)
+
+
+--List operation related functions
+
+editBoardAt :: [[a]] -> (Int,Int) -> a -> [[a]]
+editBoardAt board coords element = editRowAt board coords 0 element
+
+
+editRowAt :: [[a]] -> (Int,Int) -> Int -> a -> [[a]]
+editRowAt [] coords nRow element = []
+editRowAt (b:bs) (xCoord,yCoord) nRow element 
+	|nRow == xCoord = [(editLineAt b yCoord 0 element) ++ bs] --change element in this line and put them back together
+	|otherwise = b ++ (editRowAt bs (xCoord,yCoord) (nRow + 1) element) --Keep iterating
+
+
+editLineAt :: [a] -> Int -> Int -> a -> [a]
+editLineAt [] yCoord nColumn element = []
+editLineAt (l:ls) yCoord nColumn element 
+	|nColumn == yCoord = element : ls -- Change the element in that position
+	|otherwise = editLineAt ls yCoord (nColumn + 1) element
 
 
 main = do
