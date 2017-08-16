e73f6d24eccc921cb2751c4bc12d749f3e159032
Aramis Araujo <aramis.araujo@ccc.ufcg.edu.br>
2017-08-16 15:04:30 -0300

    Implementa sobrecarga de 'getAdjCoords' para lista de coordenadas

--- minesweeper.pl
+++ minesweeper.pl
@@ -7,8 +7,8 @@ bombTile('💣').
 emptyTile('□').
 flaggedTile('🏴').
 
-emptyTileN(0).
-bombTileN(-1).
+emptyTileNumeric(0).
+bombTileNumeric(-1).
 
 
 %Game and Display board definition
@@ -38,47 +38,68 @@ getElement([_|T], Pos, Element) :-  Z is Pos - 1, getElement(T, Z, Element).
 
 getElemAt(Board, (X,Y), Elem):- getElement(Board, Y, Row), getElement(Row, X, Elem).
 
+
 setElemAt([H|T], (Xcoord, 0), Elem, [NewRow|T]):- setEleAux(H, Elem, Xcoord, NewRow).
 setElemAt([H|T], (Xcoord, Ycoord), Elem, [H|NT]):- Z is Ycoord - 1, setElemAt(T,(Xcoord, Z), Elem, NT).
-
 
 setEleAux([_|T], Elem, 0, [Elem|T]).
 setEleAux([H|T], Elem, Pos, [H|NT]):- Z is Pos - 1, setEleAux(T, Elem, Z, NT).
 
+
 generateMine(Limit, Coord):- random_between(0,Limit,Xm), random_between(0,Limit,Ym), Coord = (Xm, Ym).
+
 
 getMines(BoardLimit, Amount, Answer):- length(Mines, Amount), 
     maplist(generateMine(BoardLimit), Mines),  sort(Mines, Answer).
 
+
 checkCoord((X, Y), Limit):- X @=< Limit, X @>= 0, Y @=< Limit, Y @>= 0.
 
-filterCoords([], Limit, []).
+
+filterCoords([], _, []).
 
 filterCoords([X|XS], Limit, [X|ZS]):- checkCoord(X, Limit),!, filterCoords(XS, Limit, ZS).
 
 filterCoords([_|XS], Limit, ZS):- filterCoords(XS, Limit, ZS).
+
+
+filterEmptyTiles([], _, _).
+filterEmptyTiles([H|T], GBoard, Answer):- getElemAt(GBoard, H, Elem), Elem =:= 0 -> Answer = [H|Ts], filterEmptyTiles(T, GBoard, Ts);
+Answer = Ts, filterEmptyTiles(T, GBoard, Ts).
+
+
+getAdjCoords([], _, []).
+getAdjCoords([H|T], Limit, AllAdjCoords):- getAdjCoords(H, Limit, FirstGroup), getAdjCoords(T, Limit, Coords),
+	sort([FirstGroup|Coords], Result), flatten(Result, AllAdjCoords). 
 
 getAdjCoords((X, Y), Limit, AdjacentCoords):- 
 	 Coords = [(A, Y), (B, Y), (X, C), (X, D), (A, C), (A, D), (B, C), (B, D)],
 	  A is X + 1, B is X - 1, C is Y + 1, D is Y - 1,
 	  filterCoords(Coords, Limit, AdjacentCoords).
 
+/**
+getAdjEmpty(AdjEmpTiles, (X,Y), GameBoard):- length(Board, Len), getAdjCoords((X,Y), Len, Board, Coords),
+ getAdjIfEmpty(GameBoard, AdjEmpTiles, Coords).
 
+getAdjIfEmpty(GameBoard, Results, []).
 
-%getAdjEmpty(AdjEmpSpaces, (X,Y), Board):-
+getAdjIfEmpty(GameBoard, Results, ToCheck):- 
+**/
+%openTiles(GameBoard, Display, [Hcoords|Tcoords]):-
 
 
 
 
 main :- 
-
+/**
 displayBoard(Camp, 9),
 setElemAt(Camp, (4, 2), 'V', Answ),
 printBoard(Answ), nl, nl,
-getMines(9, 9, Answer),
+getMines(8, 9, Answer),
 filterCoords([(1, 3), (12, 9), (4, 5)], 9, ValidCoords),
 write(ValidCoords),nl,
-write(Answer).
-
+write(Answer),**/
+getAdjCoords([(0,0), (5,5),(8,8)], 8, Coords),
+write(Coords).
 
 %halt(0).
