7de420d3c2b4dc376c7e4bda66052eee9ac90ad7
Aramis Araujo <aramis.araujo@ccc.ufcg.edu.br>
2017-08-17 20:58:15 -0300

    Necessário verificar predicados

--- minesweeper.pl
+++ minesweeper.pl
@@ -13,33 +13,34 @@ bombTileNumeric(-1).
 
 %Game and Display board definition
 gameBoard(Board, Size):- length(Board,Size), length(Row,Size),
- maplist( =(0), Row),  maplist( =(Row), Board).
+	maplist( =(0), Row),  maplist( =(Row), Board).
 
 displayBoard(Display, Size):- length(Display, Size), length(Row, Size),
-coveredTile(X), maplist( =(X), Row), maplist( =(Row), Display).
+	coveredTile(X), maplist( =(X), Row), maplist( =(Row), Display).
 
 
-%Board printing
+%Board Printing
 printBoard(Board):- write('          	0   1   2   3   4   5   6   7   8'),nl,
 					write('          	▼   ▼   ▼   ▼   ▼   ▼   ▼   ▼   ▼'), nl, nl,
 					length(Board, Length),Z is Length - 1, printAux(Board, 0, 0, Z).
 
-printAux(Board, 0, Ycoord, Limit):- getElemAt(Board, (Xcoord, Ycoord), Elem), write('           '),write(Ycoord),write(' ▶  '),
- write(Elem), write('   '), NextRow is Xcoord + 1, printAux(Board, NextRow, Ycoord, Limit).
+printAux(Board, 0, Ycoord, Limit):- getElemAt(Board, (Xcoord, Ycoord), Elem), 
+	write('           '),write(Ycoord),write(' ▶  '),
+	write(Elem), write('   '), NextRow is Xcoord + 1, printAux(Board, NextRow, Ycoord, Limit).
 
 
 printAux(Board, Limit, Limit, Limit):- getElemAt(Board, (Limit, Limit), Elem), write(Elem), nl.
 
 printAux(Board, Limit, Ycoord, Limit):- getElemAt(Board, (Limit, Ycoord), Elem),
- write(Elem), nl, nl, NextColumn is Ycoord + 1, printAux(Board, 0, NextColumn, Limit).
+	write(Elem), nl, nl, NextColumn is Ycoord + 1, printAux(Board, 0, NextColumn, Limit).
 
 printAux(Board, Xcoord, Ycoord, Limit):- getElemAt(Board, (Xcoord, Ycoord), Elem),
- write(Elem), write('   '), NextRow is Xcoord + 1, printAux(Board, NextRow, Ycoord, Limit).
+	write(Elem), write('   '), NextRow is Xcoord + 1, printAux(Board, NextRow, Ycoord, Limit).
 
 
 
 %Board Operations
-getElement([H|_], 0, H).
+getElement([H|_], 0, H):- !.
 getElement([_|T], Pos, Element) :-  Z is Pos - 1, getElement(T, Z, Element).
 
 
@@ -56,8 +57,8 @@ setEleAux([H|T], Elem, Pos, [H|NT]):- Z is Pos - 1, setEleAux(T, Elem, Z, NT).
 generateMine(Limit, Coord):- random_between(0,Limit,Xm), random_between(0,Limit,Ym), Coord = (Xm, Ym).
 
 
-getMines(BoardLimit, Amount, Answer):- length(Mines, Amount), 
-    maplist(generateMine(BoardLimit), Mines),  sort(Mines, Answer).
+getMines(BoardLimit, Amount, Answer):- length(Mines, Amount),
+	maplist(generateMine(BoardLimit), Mines),  sort(Mines, Answer).
 
 
 checkCoord((X, Y), Limit):- X @=< Limit, X @>= 0, Y @=< Limit, Y @>= 0.
@@ -71,34 +72,74 @@ filterCoords([_|XS], Limit, ZS):- filterCoords(XS, Limit, ZS).
 
 
 filterEmptyTiles([], _, _).
-filterEmptyTiles([H|T], GBoard, Answer):- getElemAt(GBoard, H, Elem), Elem =:= 0 -> Answer = [H|Ts], filterEmptyTiles(T, GBoard, Ts);
-Answer = Ts, filterEmptyTiles(T, GBoard, Ts).
+filterEmptyTiles([H|T], GBoard, Answer):- getElemAt(GBoard, H, Elem), 
+	Elem =:= 0 -> Answer = [H|Ts], filterEmptyTiles(T, GBoard, Ts);
+	Answer = Ts, filterEmptyTiles(T, GBoard, Ts).
 
 
 getAdjCoords([], _, []).
-getAdjCoords([H|T], Limit, AllAdjCoords):- getAdjCoords(H, Limit, FirstGroup), getAdjCoords(T, Limit, Coords),
-	 flatten([FirstGroup|Coords], Flat), sort(Flat, AllAdjCoords). 
+getAdjCoords([H|T], Limit, AllAdjCoords):- getAdjCoords(H, Limit, FirstGroup), 
+	getAdjCoords(T, Limit, Coords),
+	flatten([FirstGroup|Coords], Flat), sort(Flat, AllAdjCoords).
+
 getAdjCoords((X, Y), Limit, AdjacentCoords):- 
-	 Coords = [(A, Y), (B, Y), (X, C), (X, D), (A, C), (A, D), (B, C), (B, D)],
-	  A is X + 1, B is X - 1, C is Y + 1, D is Y - 1,
-	  filterCoords(Coords, Limit, AdjacentCoords).
+	Coords = [(A, Y), (B, Y), (X, C), (X, D), (A, C), (A, D), (B, C), (B, D)],
+	A is X + 1, B is X - 1, C is Y + 1, D is Y - 1,
+	filterCoords(Coords, Limit, AdjacentCoords).
 
 
-getAdjEmpty(AdjEmpTiles, (X,Y), GBoard):- length(Board, Len), getAdjCoords((X,Y), Len, Board, Coords),
- getAdjIfEmpty(GBoard, AdjEmpTiles, Coords).
+getAdjEmpty((X,Y), GBoard, AdjEmpTiles):- length(GBoard, Len), getAdjCoords((X,Y), Len, Coords),
+ getAdjIfEmpty(GBoard, Coords, [(X,Y)|AdjEmpTiles]).
 
-getAdjIfEmpty(_, _, []).
+getAdjIfEmpty(_, [], Results).
 
-getAdjIfEmpty(GBoard, Results, ToCheck):- length(GBoard, Len),
- filterEmptyTiles(ToCheck, GBoard, CheckFiltered),
- flatten([Results|CheckFiltered], FlatResults), sort(FlatResults, NewResults),
- getAdjCoords(ToCheck, Len, AdjUnchecked), sort(AdjUnchecked, AdjToCheck),
- filterEmptyTiles(AdjToCheck, GBoard, Checking), subtract(Checking, NewResults, NewToCheck),
- getAdjIfEmpty(GBoard, NewResults, NewToCheck).
+getAdjIfEmpty(GBoard, ToCheck, Results):- length(GBoard, Len),
+	filterEmptyTiles(ToCheck, GBoard, CheckFiltered),
+	flatten([Results|CheckFiltered], FlatResults), sort(FlatResults, NewResults),
 
-%openTiles(GBoard, Display, [Hcoords|Tcoords]):-
+	getAdjCoords(ToCheck, Len, AdjUnchecked), sort(AdjUnchecked, AdjToCheck),
+	filterEmptyTiles(AdjToCheck, GBoard, Checking), subtract(Checking, NewResults, NewToCheck),
+
+	getAdjIfEmpty(GBoard, NewToCheck, NewResults).
+
+openTiles(_,_, [],_).
+openTiles(GBoard, Display, [Hcoords|Tcoords], NewDisplay):- getElemAt(Display, Hcoords, ElemD),
+	coveredTile(X), 
+	X \== ElemD -> openTiles(GBoard, Display, Tcoords, NewDisplay);
+
+	getElemAt(GBoard, Hcoords, ElemB),
+	emptyTileNumeric(ElemB) -> emptyTile(Y), setElemAt(Display, Hcoords, Y, TempDisplay),
+	openTiles(GBoard, TempDisplay, Tcoords, NewDisplay);
+
+	getElemAt(GBoard, Hcoords, ElemB),
+	ElemB > 0 -> setElemAt(Display, Hcoords, ElemB, TempDisplay), 
+	openTiles(GBoard, TempDisplay, Tcoords, NewDisplay);
+
+	getElemAt(GBoard, Hcoords, ElemB),
+	bombTileNumeric(ElemB) -> bombTile(Z), setElemAt(Display, Hcoords, Z, TempDisplay),
+	openTiles(GBoard, TempDisplay, Tcoords, NewDisplay).
+
+applyBombs([T], GBoard, ResultBoard):- bombTileNumeric(X),length(GBoard, Len),
+	setElemAt(GBoard, T, X, TempBoard), getAdjCoords(T, Len, AdjCoords),
+	addTip(TempBoard, AdjCoords, ResultBoard).
 
 
+applyBombs([Hbomb|Tbomb], GBoard, ResultBoard):-bombTileNumeric(X),length(GBoard, Len),
+	setElemAt(GBoard, Hbomb, X, TempBoard), getAdjCoords(Hbomb, Len, AdjCoords),
+	addTip(TempBoard, AdjCoords, TempBoard2), applyBombs(Tbomb, TempBoard2, ResultBoard).
+
+addTip(GBoard, [T], ResultBoard):- getElemAt(GBoard, T, X),
+	X @>= 0 -> Z is X + 1, setElemAt(GBoard, T, Z, ResultBoard). 
+
+addTip(GBoard, [Hcoord|Tcoords], ResultBoard):- getElemAt(GBoard, Hcoord, X),
+	X @>= 0 -> Z is X + 1, setElemAt(GBoard, Hcoord, Z, TempBoard),
+	addTip(TempBoard, Tcoords, ResultBoard);
+	X @< 0 -> addTip(GBoard, Tcoords, ResultBoard). 
+
+
+%getHintCoords
+
+%User interaction
 header(Board) :- 
 	nl, nl, nl,
 	write("			## Minesweeper##"), nl, nl,
@@ -112,7 +153,6 @@ getOption(Option):-
 	write("Please, type a valid option: "), readNum(Op),
 	Op @> 0 ->
 		Option = Op;
-	Op @=< 0 ->
 		write("	Your option is not valid!"),nl, getOption(Option).
 
 getCoords(Size, (X, Y)):-
@@ -130,6 +170,15 @@ readNum(X):- read_line_to_codes(user_input,A2),
 
 main :- 
 
+	gameBoard(GB, 9),
+	printBoard(GB),nl,
+	getMines(8, 9, BombList),
+	write(BombList),nl,
+	%addTip(GB, [(5,5),(6,6),(7,7)], Result),
+	applyBombs(BombList, GB, Result),
+	printBoard(Result),
+	getAdjEmpty((8,3), Coords, Result), write(Coords).
+/**
 	displayBoard(Camp, 9),
 	bombTile(X),
 	setElemAt(Camp, (4, 2), X, Answ),
@@ -141,5 +190,5 @@ main :-
 	write(Answer),
 	getAdjCoords([(0,0), (1,1)], 8, Coords),
 	write(Coords).
-
+**/
 	%halt(0).
